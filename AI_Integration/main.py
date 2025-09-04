d91525669f435ffeb1133d62eeeeab18f28de5bf
nandhiraja <nandhiraja16@gmail.com>
2025-09-04 18:59:36 +0530

    Modified - Model

--- AI_Integration/main.py
+++ AI_Integration/main.py
@@ -194,7 +194,7 @@ Generate_System_Instruction = """You are an IEC 61131 Structured Text to JSON tr
 load_dotenv()
 llm = ChatGroq(
     groq_api_key=os.environ["GROQ_API_KEY3"],
-    model=os.environ.get("GROQ_MODEL_NAME", "llama-3.1-70b-versatile"),
+    model=os.environ.get("GROQ_MODEL_NAME", "openai/gpt-oss-120b"),
 )
 
 
